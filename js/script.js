eaeb8bec567b10a670d9a810704d06219a102397
Simone-Domenici <domenici.simone00@gmail.com>
2024-10-24 15:00:04 +0200

    ADD number verification

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+let correctNumbers;
+
 // Funzione per generare un array di numeri casuali unici da 1 a 50
 
 function generateRandomNumbers() {
@@ -5,9 +7,13 @@ function generateRandomNumbers() {
     while (numbers.size < 5) {
       numbers.add(Math.floor(Math.random() * 50) + 1);
     }
-    return Array.from(numbers);
+
+    correctNumbers = Array.from(numbers);
+    // console.log(correctNumbers)
+    return correctNumbers;
   }
 // console.log(generateRandomNumbers())
+
 
 // Funzione per visualizzare i numeri nel DOM
 
@@ -25,7 +31,7 @@ function startGame() {
 // Countdown
 
     const countdown = document.getElementById('countdown');
-      let timeLeft = 5;
+      let timeLeft = 30;
 
       const countdownInterval = setInterval(() => {
         countdown.textContent = timeLeft;
@@ -39,5 +45,34 @@ function startGame() {
     }, 1000);
 }
 
-startGame()
+// Funzione di controllo delle risposte
 
+function checkAnswers() {
+    const inputElements = document.querySelectorAll('#input-group input');
+    const userAnswers = Array.from(inputElements).map(input => parseInt(input.value));
+
+    let correctCount = 0;
+    const messageElement = document.getElementById('message');
+
+    if (userAnswers.some(isNaN) || new Set(userAnswers).size !== userAnswers.length) {
+        messageElement.textContent = 'Inserisci solo numeri validi e unici.';
+        return;
+    }
+
+    userAnswers.forEach(answer => {
+        if (correctNumbers.includes(answer)) {
+          correctCount++;
+        }
+    });
+
+    messageElement.textContent = `Hai indovinato ${correctCount} numeri su 5.`;
+}
+
+// Partenza del gioco all'avvio della pagina
+
+startGame();
+
+document.getElementById('answers-form').addEventListener('submit', (event) => {
+    event.preventDefault();
+    checkAnswers();
+});
